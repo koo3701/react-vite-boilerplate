4cea7bb1b40deef2e5ce62f36cdef03df86ef86f
koo3701 <koo.0107.koo@gmail.com>
2022-12-31 07:09:39 +0900

    add Windi CSS

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import WindiCSS from 'vite-plugin-windicss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), WindiCSS()],
   server: {
     host: '0.0.0.0',
     port: 3000,
@@ -12,7 +13,7 @@ export default defineConfig({
         target: 'http://server:3000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
-      }
-    }
+      },
+    },
   },
-})
+});
