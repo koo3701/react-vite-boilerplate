15ee9e7ccbfa6d79c797bf315758c8111a87fd7b
koo3701 <koo.0107.koo@gmail.com>
2022-12-31 06:37:10 +0900

    add eslint in client

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'airbnb',
+    'airbnb-typescript',
+    'prettier',
+  ],
+  overrides: [],
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+};
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,32 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import reactLogo from './assets/react.svg'
-import './App.css'
+import reactLogo from './assets/react.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const [message, setMessage] = useState<string>();
 
   useEffect(() => {
     (async () => {
-      const message = (await axios.get('/api')).data.message;
-      setMessage(message);
-    })();
+      const msg = (await axios.get<{ message: string }>('/api')).data.message;
+      setMessage(msg);
+    })().catch(() => {});
   }, []);
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button type="button" onClick={() => setCount((cnt) => cnt + 1)}>
           count is {count}
         </button>
         <p>
@@ -34,11 +34,9 @@ function App() {
         </p>
         <p>Server Response: {message ?? 'loading...'}</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
