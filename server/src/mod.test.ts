ac2f9a2a0fc0e957da7d84b2fa8232e99ffef9e3
koo3701 <koo.0107.koo@gmail.com>
2023-01-07 20:30:39 +0900

    fix test location

--- server/src/mod.test.ts
+++ server/src/mod.test.ts
@@ -1,7 +1,7 @@
 import { superoak } from "superoak";
 import { app } from "../src/mod.ts";
 
-/* ルート(/)へのリクエストのテスト */
+/* request root(/) test */
 Deno.test('it should return "Hello World!" with status code 200', async () => {
   const correct = { message: "Hello World!" };
 
