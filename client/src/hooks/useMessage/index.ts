a710dba498ec324ea93e4939ee079ee26a8fe8fe
koo3701 <koo.0107.koo@gmail.com>
2023-01-07 16:21:44 +0900

    add hooks test file

--- client/src/hooks/useMessage/index.ts
+++ client/src/hooks/useMessage/index.ts
@@ -2,9 +2,14 @@ import { useQuery } from '@tanstack/react-query';
 
 import { axios } from '@/lib/axios';
 
+/**
+ * @package
+ */
+export type MessageResponseType = { message: string };
+
 export const useMessage = () => {
   const { isLoading, error, data } = useQuery(['useMessage'], () =>
-    axios.get<{ message: string }>('/')
+    axios.get<MessageResponseType>('/')
   );
 
   return isLoading ? 'loading...' : error ? 'load error!' : data?.data.message ?? 'loading...';
---
+++ client/src/hooks/useMessage/useMessage.test.tsx
@@ -0,0 +1,82 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import { cleanup, renderHook, waitFor } from '@testing-library/react';
+import { expect, test } from 'vitest';
+
+import { MessageResponseType, useMessage } from '@/hooks/useMessage';
+
+vi.mock('@/lib/axios');
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
+
+const wrapper = ({ children }: { children: React.ReactNode }) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
+
+describe('hooks/useMessage', () => {
+  beforeEach(() => {
+    cleanup();
+    queryClient.clear();
+  });
+
+  test('success', async () => {
+    const message = 'Hello World!';
+
+    const { axios } = await import('@/lib/axios');
+
+    axios.get = vi.fn().mockResolvedValueOnce({
+      data: {
+        message,
+      } satisfies MessageResponseType,
+    });
+
+    const { result } = renderHook(() => useMessage(), {
+      wrapper,
+    });
+
+    expect(axios.get).toHaveBeenCalledWith('/');
+    expect(axios.get).toHaveBeenCalledTimes(1);
+
+    await waitFor(() => expect(result.current).toBe(message));
+  });
+
+  test('error', async () => {
+    const message = 'load error!';
+
+    const { axios } = await import('@/lib/axios');
+
+    axios.get = vi.fn().mockRejectedValueOnce({});
+
+    const { result } = renderHook(() => useMessage(), {
+      wrapper,
+    });
+
+    expect(axios.get).toHaveBeenCalledWith('/');
+    expect(axios.get).toHaveBeenCalledTimes(1);
+
+    await waitFor(() => expect(result.current).toBe(message));
+  });
+
+  test('error', async () => {
+    const message = 'loading...';
+
+    const { axios } = await import('@/lib/axios');
+
+    axios.get = vi.fn().mockReturnValueOnce(new Promise(() => {}));
+
+    const { result } = renderHook(() => useMessage(), {
+      wrapper,
+    });
+
+    expect(axios.get).toHaveBeenCalledWith('/');
+    expect(axios.get).toHaveBeenCalledTimes(1);
+
+    await waitFor(() => expect(result.current).toBe(message));
+  });
+});
